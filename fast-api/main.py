b209ed0842c96bb82ff6e6ae24ae01043a1b4fc4
tejasdhanani <tejas27dhanani@gmail.com>
2025-06-16 19:23:59 +0530

    initiated fastapi app

---
+++ fast-api/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+origins = [
+    'http://localhost:3000'
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins
+)
+
